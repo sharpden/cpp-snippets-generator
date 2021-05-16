a4a36bfd086b12fbc08e281313334277989e92a7
do <sharp-c@yandex.ru>
2021-05-16 19:38:43 +0300

    import ultra removed, added py3, Sublime, $selected$, folder override

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ Add to `snippets.txt` snippet like:
 ================================================================================
 // #shortcut
 code
+$selected$ - selected text before snippet was used
+code
 $end$ - where to place cursor
 ================================================================================
 ```
@@ -14,8 +16,11 @@ $end$ - where to place cursor
 
 Run `python snippets.py`.
 
-It will re-generate files `*.snippet` in folder `~\My Documents\Visual Studio 2019\Code Snippets\Visual C++\My Code Snippets\` for Visual Studio 2019 and file `cpp.json` in folder `~\AppData\Roaming\Code\User\snippets\` for VS Code.
+It will re-generate (all editors should automatically detect changes):
+* for **Visual Studio 2019**: files `*.snippet` in folder `~\My Documents\Visual Studio 2019\Code Snippets\Visual C++\My Code Snippets\`. See Tools > Code Snippets Manager.
+* for **VS Code**: file `cpp.json` in folder `~\AppData\Roaming\Code\User\snippets\`. See File > Preferences > User Snippets.
+* for **Sublime**: files `*.sublime-snippet` in folder `~\AppData\Roaming\Sublime Text 3\Packages\User\`. See Tools > Snippets.
 
-Visual Studio automatically detects new files `*.snippet`. If no, look Tools > Code Snippets Manager.
+Paths could be overriden in `settings.py` file, `settings.py-sample` included.
 
 Now you can start print `#shortcut` in IDE and press Tab when menu appears. Snippet name will be expanded to full code.
--- snippets.py
+++ snippets.py
@@ -1,10 +1,44 @@
-from ultra import *
+from __future__ import print_function
+import json
+import os
+import sys
+
+from settings import folders
+
+
+def S(s):
+    if sys.version_info.major == 3:
+        return s.decode('utf-8')
+    return s
+
+def B(s):
+    if sys.version_info.major == 3:
+        return s.encode('utf-8')
+    return s
+
+def file_put(filename, text):
+    with open(filename, 'wb') as fp:
+        fp.write(B(text))
+
+def file_get(filename):
+    with open(filename, 'rb') as fp:
+        return S(fp.read())
 
 
 sn = file_get('snippets.txt')
 
 # Visual Studio
-fmt = '''\
+# https://docs.microsoft.com/en-us/visualstudio/ide/code-snippets?view=vs-2019
+# May support placeholders like $Width$ by
+# <Declarations>
+#   <Literal>
+#     <ID>Width</ID>
+#     <Type>Integer</Type>
+#     <ToolTip>Replace by width</ToolTip>
+#     <Default>150</Default>
+#   </Literal>
+# </Declarations>
+VS_fmt = '''\
 <?xml version="1.0" encoding="utf-8"?>
 <CodeSnippets xmlns="http://schemas.microsoft.com/VisualStudio/2005/CodeSnippet">
     <CodeSnippet Format="1.0.0">
@@ -24,6 +58,7 @@ fmt = '''\
 '''
 
 # VSCode
+# https://code.visualstudio.com/docs/editor/userdefinedsnippets
 '''
 // Place your snippets for cpp here. Each snippet is defined under a snippet name and has a prefix, body and 
 // description. The prefix is what is used to trigger the snippet and the body will be expanded and inserted. Possible variables are:
@@ -40,6 +75,20 @@ fmt = '''\
 // }
 '''
 
+# Sublime
+# https://docs.sublimetext.io/guide/extensibility/snippets.html
+# ${1:this} like in VS Code are supported
+Sublime_fmt = '''\
+<snippet>
+	<tabTrigger>%s</tabTrigger>
+	<content><![CDATA[%s]]></content>
+</snippet>
+'''
+
+# TODO:
+# IDEA: File | Settings | Live Templates; https://www.jetbrains.com/help/idea/using-live-templates.html
+# Notepad++: No support yet https://github.com/ffes/nppsnippets/issues/33
+
 
 result = {}
 
@@ -48,27 +97,37 @@ for note in sn.split('=' * 80 + '\r\n'):
     body = '\r\n'.join(note.splitlines()[1:])
     if head.startswith('// '):
         short = head[3:]
-        print '[%s]' % short
-        print note
-        print '-' * 80
+        print('[%s]' % short)
+        print(note)
+        print('-' * 80)
         result[short[1:]] = body
 
 
-for k, v in result.items():
-    fn = os.path.expanduser('~\\Documents\\Visual Studio 2019\\Code Snippets\\Visual C++\\My Code Snippets\\%s.snippet' % k)
-    content = fmt % ('#' + k, '#' + k, v)
-    if not os.path.exists(fn) or file_get(fn) != v:
+if 'VS' in folders:
+    for k, v in result.items():
+        fn = os.path.expanduser(os.path.join(folders['VS'], '%s.snippet' % k))
+        content = VS_fmt % ('#' + k, '#' + k, v)
+        if not os.path.exists(fn) or file_get(fn) != content:
+            file_put(fn, content)
+
+
+if 'VSCODE' in folders:
+    fn = os.path.expanduser(os.path.join(folders['VSCODE'], 'cpp.json'))
+    content = {}
+    for k, v in result.items():
+        content[k] = {
+            'prefix': '#' + k,
+            'body': v.replace('$end$', '$0').replace('$selected$', '$TM_SELECTED_TEXT').splitlines(),
+            'description': k,
+        }
+    content = json.dumps(content)
+    if not os.path.exists(fn) or file_get(fn) != content:
         file_put(fn, content)
 
 
-fn = os.path.expanduser('~\\AppData\\Roaming\\Code\\User\\snippets\\cpp.json')
-content = {}
-for k, v in result.items():
-    content[k] = {
-        'prefix': '#' + k,
-        'body': v.replace('$end$', '$0').splitlines(),
-        'description': k,
-    }
-content = json.dumps(content)
-if not os.path.exists(fn) or file_get(fn) != content:
-    file_put(fn, content)
+if 'SUBLIME' in folders:
+    for k, v in result.items():
+        fn = os.path.expanduser(os.path.join(folders['SUBLIME'], '%s.sublime-snippet' % k))
+        content = Sublime_fmt % ('#' + k, v.replace('$end$', '$0').replace('$selected$', '$SELECTION'))
+        if not os.path.exists(fn) or file_get(fn) != content:
+            file_put(fn, content)
